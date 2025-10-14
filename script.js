1bd8775e92010e6bd56724987a7fe2e8fd000ab6
AFTHAB KHAN A <afthabkhankgl@gmail.com>
2025-10-15 02:09:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#element', {
+            strings: ['<i>Web</i> developer','<div>&amp;<i> UI/UX</i> designer'],
+            typeSpeed: 50,
+            loop: true
+        });
+
